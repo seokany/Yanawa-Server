394d9ddc19dd37346c0ce1f18ffdd3e821373195
youngjjjj <>
2020-07-22 22:17:46 +0900

    ADD signup view

---
+++ accounts/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "pk",
+            "username",
+            "user_image",
+        ]
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.SerializerMethodField(write_only=True)
+
+    def save(self, **kwargs):
+        user = User.objects.create(
+            email=self.validated_data["email"],
+            username=self.validated_data["username"],
+        )
+
+        if len(self.validated_data["password"]) >= 8:
+            user.set_password(self.validated_data["password"])
+            user.save()
+            return user
+        else:
+            raise ValidationError("패스워드를 8글자 이상 입력해주세요")
+
+    class Meta:
+        model = User
+        fields = [
+            "pk",
+            "email",
+            "username",
+            "user_image",
+            "password",
+        ]
